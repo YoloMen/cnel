0ccaf594c81f074270615788e42a7a25d5d200b1
Jaime Vilca <jaime_v@outlook.com>
2016-01-08 14:38:18 -0500

    Modificación del modelo de conexión

--- libs/Model.php
+++ libs/Model.php
@@ -1,10 +1,26 @@
 <?php
 
 Class Model{
+
+	private static $_instance;
     
-	public function __construct(){
-            $this->db = new PDOManager('mysql', 'localhost', 'root', '', 'ssp_test');
+	private function __construct(){
+        //$this->db = new PDOManager('mysql', 'localhost', 'root', '', 'ssp_test');
 	}
+
+	public static function getInstance(){
+ 
+        if(!isset(self::$_instance)){
+             self::$_instance = new PDOManager('mysql', 'localhost', 'root', '', 'ssp_test');
+        }
+ 
+        if(!isset(self::$_instance)){
+            throw new Exception('Database connection cannot be established');
+        }
+ 
+        return self::$_instance;
+ 
+    }
 }
 
 ?>
